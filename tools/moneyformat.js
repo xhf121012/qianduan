239409ed3cce9d70e86c62f80f35f4344fdbfe5f
xiehaifeng <xiehaifeng@autohome.com.cn>
2020-01-03 13:45:02 +0800

    格式化代码

--- tools/moneyformat.js
+++ tools/moneyformat.js
@@ -1,15 +1,35 @@
-let num = 12346123;
+let num = 1234453256123;
 
 function format(num) {
     if (typeof num !== "number") {
         return num;
     }
+    
     let str = num.toString();
     let mod = str.length % 3
+
     for (let i = 0; i < 3 - mod; i++) {
         str = "0" + str;
     }
+
     str = str.match(/\d{3}/g).join(",");
     return str.replace(/^0+/, "");
 }
-console.log(format(num));
+
+function format1(num) {
+    if (typeof num !== "number") {
+        return num;
+    }
+
+    let str = num.toString();
+    let array = [];
+    let mod = str.length % 3;
+    array.push(str.substr(0, mod));
+    str.substr(0, mod);
+
+    for (let i = mod; i < str.length; i += 3) {
+        array.push(str.substr(i, 3));
+    }
+    return array.join(",");
+}
+console.log(format1(num));
